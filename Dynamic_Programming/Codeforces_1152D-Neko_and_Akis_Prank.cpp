2504f63385387132c0d470ccc3fd34d2b2fd38f6
Md Al Imran <imransci143@gmail.com>
2019-12-09 09:53:41 +0600

    Update Codeforces_1152D-Neko_and_Akis_Prank.cpp

--- Dynamic_Programming/Codeforces_1152D-Neko_and_Akis_Prank.cpp
+++ Dynamic_Programming/Codeforces_1152D-Neko_and_Akis_Prank.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 const int maxn = 101010;
 const long long mod = 1e9 + 7;
-
+ 
 int n;
 bool mark[2][1010][1010];
 long long dp[2][1010][1010];
-
+ 
 long long solve(int isGood, int x, int y)
 {
     if ((x == y && x == n) || y > x || x > n || y > n)
         return 0;
-
+ 
     if (mark[isGood][x][y])
         return dp[isGood][x][y];
-
+ 
     mark[isGood][x][y] = true;
-
+ 
     if (isGood)
-    {
         dp[isGood][x][y] = (1 + solve(0, x + 1, y) % mod + solve(0, x, y + 1) % mod) % mod;
-        dp[isGood][x][y] = max(dp[isGood][x][y], 1 + (solve(0, x + 1, y) % mod + solve(0, x, y + 1) % mod) % mod);
-    }
     else
         dp[isGood][x][y] = (solve(1, x + 1, y) % mod + solve(1, x, y + 1) % mod) % mod;
-
+ 
     return dp[isGood][x][y];
 }
-
+ 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
+ 
     cin >> n;
-
+ 
     cout << solve(0, 0, 0) % mod << endl;
-
+ 
     return 0;
 }
